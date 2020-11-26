bba6ed7929f213a0056e8f09f653cf8cd90a18ee
nougat11 <30313702+nougat11@users.noreply.github.com>
2020-11-26 19:24:11 +0300

    fix: OCP violation

--- Task1/main.py
+++ Task1/main.py
@@ -7,21 +7,26 @@ from file_writers import JsonWriter, XmlWriter
 def parse_args():
     """This method parse args from console"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', '--rooms', help="Path for rooms json file")
-    parser.add_argument('-s', '--students', help="Path for students json file")
-    parser.add_argument('-f', '--format', choices=['json', 'xml'], help="Format output file")
+    parser.add_argument("-r", "--rooms", help="Path for rooms json file")
+    parser.add_argument("-s", "--students", help="Path for students json file")
+    parser.add_argument(
+        "-f", "--format", choices=["json", "xml"], help="Format output file"
+    )
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+format_classes = {"json": JsonWriter(), "xml": XmlWriter()}
+
+
+if __name__ == "__main__":
     args = parse_args()
     list_of_rooms = JsonLoader(args.rooms).load_json()
     list_of_students = JsonLoader(args.students).load_json()
-    hostel = {room['id']: {'room': room['name'], 'students': []} for room in list_of_rooms}
+    hostel = {
+        room["id"]: {"room": room["name"], "students": []} for room in list_of_rooms
+    }
     for student in list_of_students:
-        hostel[student['room']]['students'].append(student['name'])
+        hostel[student["room"]]["students"].append(student["name"])
     hostel = [item[1] for item in hostel.items()]
-    if args.format == 'json':
-        JsonWriter().write(hostel)
-    if args.format == 'xml':
-        XmlWriter().write(hostel)
+    file_writer = format_classes[args.format]
+    file_writer.write(hostel)
